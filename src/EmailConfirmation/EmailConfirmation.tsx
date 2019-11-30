41e9f53248cb563849ad5fa1b11eaee2e1428755
Shaharamir <shahramir3@gmail.com>
2019-11-30 02:18:45 +0200

    * Added resend email option

--- src/EmailConfirmation/EmailConfirmation.tsx
+++ src/EmailConfirmation/EmailConfirmation.tsx
@@ -3,7 +3,8 @@ import { css, jsx } from '@emotion/core';
 import React from 'react';
 import { Container } from '../shared';
 import { IDatabaseUser } from '../types/types';
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
+import axios from 'axios';
 
 interface IProps {
     user: IDatabaseUser
@@ -18,11 +19,21 @@ const EmailConfirmation: React.FC<IProps> = (props) => {
       margin-bottom: 0.2em;
     `;
 
+    const resendEmail = () => {
+      axios.post('http://localhost:8080/user/resendEmailConfirmation/', user)
+      .then((res) => {
+        console.log(res.data);
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
+
   return (
     <Container>
         <Typography variant="h4" css={header}>Verify Email</Typography>
         <div>Hey <b>{user.username}</b>,</div>
-        <div>Please verify your email at <b>{user.email}</b></div>
+        <div>Please verify your email adress: <b>{user.email}</b></div>
+        <div css={css`margin-top: 1em;`}>Didn't recieve the mail? <Button variant="outlined" css={css`margin-left: 1em;`} onClick={resendEmail}>resend it now</Button></div>
     </Container>
   );
 }
