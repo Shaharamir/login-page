86869c6b8a77fda91fbbfe5d5fff8ef162162ba5
Shaharamir <shahramir3@gmail.com>
2019-12-25 20:40:54 +0200

    removed why did you render

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { StylesProvider } from '@material-ui/core/styles';
 
-if (process.env.NODE_ENV !== 'production') {
-    const whyDidYouRender = require('@welldone-software/why-did-you-render');
-    whyDidYouRender(React);
-}
-
 ReactDOM.render(<StylesProvider injectFirst><App /></StylesProvider>, document.getElementById('root'));
