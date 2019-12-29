dcc6f276710b8b6666e8b3353da7b5b1a92b2640
Shahar <shahramir3@gmail.com>
2019-12-29 17:37:41 +0200

    Updates

---
+++ precache-manifest.af2c74b3fea10a0c22455ce3891761d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ea834ca0a77fdea2b294c9511de8c3",
+    "url": "/login-page-client/index.html"
+  },
+  {
+    "revision": "a3acdd6c7b99ee88b382",
+    "url": "/login-page-client/static/js/2.848a8859.chunk.js"
+  },
+  {
+    "revision": "2450ce52c70041ec9bc5",
+    "url": "/login-page-client/static/js/main.f3de8f4b.chunk.js"
+  },
+  {
+    "revision": "9f4725188373333a922e",
+    "url": "/login-page-client/static/js/runtime-main.5e9b3f80.js"
+  }
+]);
